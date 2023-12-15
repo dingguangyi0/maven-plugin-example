8cedd9e26ab63700cbdc7c2d10a0a2ce6125a58f
guangyi.ding <guangyi.ding@77hub.com>
2023-12-15 11:42:00 +0800

    初始化项目

---
+++ YcExample-maven-plugin/src/main/java/fun/ycdr/example/YcMojo.java
@@ -0,0 +1,44 @@
+package fun.ycdr.example;
+
+/*
+ * Copyright 2001-2005 The Apache Software Foundation.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import org.apache.maven.plugin.AbstractMojo;
+import org.apache.maven.plugin.MojoExecutionException;
+import org.apache.maven.plugin.MojoFailureException;
+import org.apache.maven.plugins.annotations.LifecyclePhase;
+import org.apache.maven.plugins.annotations.Mojo;
+import org.apache.maven.plugins.annotations.Parameter;
+
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+
+
+@Mojo(
+        name = "msg",
+        defaultPhase = LifecyclePhase.INITIALIZE
+)
+public class YcMojo extends AbstractMojo {
+
+    @Parameter
+    private String msg;
+
+    @Override
+    public void execute() throws MojoExecutionException, MojoFailureException {
+        getLog().info("来自外界的消息-{"+msg+"}");
+    }
+}
